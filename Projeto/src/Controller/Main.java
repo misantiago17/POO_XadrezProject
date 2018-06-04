7529b4ecb1b4fa1cea52698020cd4988000aad16
Michelle Santiago Beadle <michelle.santiago10@gmail.com>
2018-06-04 18:40:15 -0300

    Menu inicializa o jogo

--- Projeto/src/Controller/Main.java
+++ Projeto/src/Controller/Main.java
@@ -7,12 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// Inicializa o menu
-		//Interface menu = new Menu();
-		//menu.cria();
+		Interface menu = new Menu();
+		menu.cria();
 		
 		// Inicializa Xadrez
-		Interface xadrez = new XadrezFrame();
-		xadrez.cria();
+		//Interface xadrez = new XadrezFrame();
+		//xadrez.cria();
 	}
 
 }
--- Projeto/src/Interface/Menu.java
+++ Projeto/src/Interface/Menu.java
@@ -1,26 +1,63 @@
 package Interface;
 
 import javax.swing.*;
+import javax.swing.event.PopupMenuListener;
+
+import com.sun.glass.events.WindowEvent;
+
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //Primeiro menu do jogo
-public class Menu extends Interface {
+public class Menu extends Interface implements ActionListener {
 	
 	private final int LARGURA = 400;
 	private final int ALTURA = 400;
 	private final String NOME = "MENU";
 	
+	private JFrame _f;
+	
 	public void cria() {
-		JFrame f = criaJanela(ALTURA,LARGURA,NOME);
-		JPanel p = criaPainel(Color.WHITE,f);
-		JButton b1 = criaBotao("Jogar", p);
-		JButton b2 = criaBotao("Carregar Jogo", p);
+		_f = criaJanela(ALTURA,LARGURA,NOME);
+		JPanel p = criaPainel(Color.WHITE,_f);
 		
 		p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
-		b1.setAlignmentX(Component.CENTER_ALIGNMENT);
-		b2.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
+		JButton b1 = criaBotao("Jogar", p);		
+		b1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		b1.setActionCommand("Jogar");
+		b1.addActionListener(this);
+
+		JButton b2 = criaBotao("Carregar Jogo", p);
+		b2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		b2.setActionCommand("Load");
+		b2.addActionListener(this);
+
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		JButton button = (JButton) e.getSource();
+	    String command = button.getActionCommand();
+	    
+	    if (command == "Jogar") {
+	    	
+	    	// Inicializa Xadrez
+			Interface xadrez = new XadrezFrame();
+			xadrez.cria();
+			
+			// fecha a tela anterior
+			 _f.setVisible(false);
+			 dispose();
+			
+	    	
+	    } else {
+	    	// Carrega jogo salvo
+	    }
 	}
 
 }
